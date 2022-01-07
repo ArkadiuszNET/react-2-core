08c64b647383d70fba83158f94542de79165b31f
Arkadiusz <system252@interia.pl>
2022-01-07 18:33:34 +0100

    Decreased size of container image and handling env variables

--- Ignition/Frontend/client/src/App.tsx
+++ Ignition/Frontend/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>App env: {process.env.REACT_APP_NAME}</p>
+        <p>App Name: {process.env.REACT_APP_NAME}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
